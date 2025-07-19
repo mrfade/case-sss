d9b3936fb30a298d3c6ad8e15022affd4aefb7ac
Enes Solak <enes@solak.dev>
2025-07-19 03:52:01 +0300

    fix: add json tags to content model

--- internal/core/models/content.go
+++ internal/core/models/content.go
@@ -10,16 +10,16 @@ const (
 )
 
 type Content struct {
-	ID          int64       `gorm:"primaryKey,autoIncrement"`
-	Provider    string      `gorm:"not null"`
-	ProviderID  string      `gorm:"not null"`
-	Title       string      `gorm:"not null"`
-	Type        ContentType `gorm:"not null"`
-	Tags        []string    `gorm:"serializer:json;null"`
-	Views       int         `gorm:"default:0"`
-	Likes       int         `gorm:"default:0"`
-	ReadingTime int         `gorm:"default:0"`
-	Reactions   int         `gorm:"default:0"`
-	PublishedAt time.Time   `gorm:"not null"`
-	Score       float64     `gorm:"default:0"`
+	ID          int64       `gorm:"primaryKey,autoIncrement" json:"id"`
+	Provider    string      `gorm:"not null" json:"provider"`
+	ProviderID  string      `gorm:"not null" json:"provider_id"`
+	Title       string      `gorm:"not null" json:"title"`
+	Type        ContentType `gorm:"not null" json:"type"`
+	Tags        []string    `gorm:"serializer:json;null" json:"tags"`
+	Views       int         `gorm:"-:all" json:"-"`
+	Likes       int         `gorm:"-:all" json:"-"`
+	ReadingTime int         `gorm:"-:all" json:"-"`
+	Reactions   int         `gorm:"-:all" json:"-"`
+	PublishedAt time.Time   `gorm:"not null" json:"published_at"`
+	Score       float64     `gorm:"default:0" json:"score"`
 }
