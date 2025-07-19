58e967a8005e8ca58aa0c5bca8c02a298779263b
Enes Solak <enes@solak.dev>
2025-07-19 03:46:09 +0300

    feat: implement capture request for gin

---
+++ pkg/request/gin.go
@@ -0,0 +1,52 @@
+package request
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CaptureGinRequest(c *gin.Context) *Request {
+	pageNumber := 1
+	pageSize := 10
+
+	page, _ := c.GetQueryMap("page")
+	if p, ok := page["number"]; ok {
+		intPageNumber, err := strconv.Atoi(p)
+		if err == nil {
+			pageNumber = intPageNumber
+		}
+	}
+	if p, ok := page["size"]; ok {
+		intPageSize, err := strconv.Atoi(p)
+		if err == nil {
+			pageSize = intPageSize
+		}
+	}
+
+	sorts := map[string]string{}
+	sortString := c.Query("sort")
+	sortSlice := strings.Split(strings.TrimSpace(sortString), ",")
+	for _, sort := range sortSlice {
+		if len(sort) == 0 {
+			continue
+		}
+
+		// if it has a "-" prefix, it means it is a descending sort
+		if strings.HasPrefix(sort, "-") {
+			sorts[strings.TrimPrefix(sort, "-")] = "desc"
+		} else {
+			sorts[sort] = "asc"
+		}
+	}
+
+	search := c.Query("search")
+
+	return &Request{
+		PageNumber: pageNumber,
+		PageSize:   pageSize,
+		Sorts:      sorts,
+		Search:     search,
+	}
+}
