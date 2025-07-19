ea7783f78957e12eba1f360343bf0c3b9b25a514
Enes Solak <enes@solak.dev>
2025-07-19 03:45:16 +0300

    fix: add additional error messages for providers

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -5,7 +5,17 @@ import errs "errors"
 var (
 	// Database errors
 	ErrUnableToConnectDB = errs.New("unable to connect to the database")
+	ErrNotFound          = errs.New("record not found")
+	ErrConflictingData   = errs.New("conflicting data")
 
 	// Redis errors
 	ErrUnableToConnectRedis = errs.New("unable to connect to Redis")
+
+	// JSON Provider errors
+	ErrJSONProviderRequestFailed = errs.New("JSON provider failed to fetch data")
+	ErrJSONProviderDecodeFailed  = errs.New("JSON provider failed to decode response")
+
+	// XML Provider errors
+	ErrXMLProviderRequestFailed = errs.New("XML provider failed to fetch data")
+	ErrXMLProviderDecodeFailed  = errs.New("XML provider failed to decode response")
 )
